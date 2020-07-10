dbccc748a7fd59e8e920f7fabb80b359d7a2405f
DiriectorDoc <66105586+DiriectorDoc@users.noreply.github.com>
2020-07-10 00:20:16 -0400

    Add files via upload
    Adjusted function names to fit file name. Also, in exportBoard.js, added the ability to export notes.

--- exportBoard.js
+++ exportBoard.js
@@ -1,40 +1,41 @@
 (function(root, factory){
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['exportList'], function (a) {
-            return (root.amdWebGlobal = factory(a));
-        });
-    } else {
-        // Browser globals
-        root.exportList = factory(root.exportList);
-    }
-}(typeof window !== 'undefined' ? window : this, function(exportList){
-    
-	exportList = function(list){
-		if(list){
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['exportBoard'], function (a) {
+			return (root.amdWebGlobal = factory(a));
+		});
+	} else {
+		// Browser globals
+		root.exportBoard = factory(root.exportBoard);
+	}
+}(typeof window !== 'undefined' ? window : this, function(exportBoard){
+
+	exportBoard = function(board){
+		if(board){
 			let name;
-			if(typeof list === "object"){
-				name = list.title;
-				list = JSON.stringify(list, null, 4)
-			} else if(typeof list === "string"){
-				name = (/"title":\s*"(.*)",?/g).exec(list)[1];
+			if(typeof board === "object"){
+				name = board.title;
+				board = JSON.stringify(board, null, 4)
+			} else if(typeof board === "string"){
+				name = (/"title":\s*"(.*)",?/g).exec(board)[1];
 			}
 			$("<a>").attr({
 				download: name,
-				href: URL.createObjectURL(new Blob([list], {type : 'application/json'}))
+				href: URL.createObjectURL(new Blob([board], {type : 'application/json'}))
 			})[0].click();
 			return;
 		} else {
-			list = {
-				version: 0.0106,
+			board = {
+				version: 0.0107,
 				title: "Checklist",
-				lists: []
+				lists: [],
+				notes: []
 			};
-			
+
 			$(".list").toArray().forEach(function(e){
 				e = $(e)[0];
 				let n = (/list-(\d+)/g).exec(e.id)[1] * 1;
-				
+
 				let newList = {
 					name: $("#list-" + n + "-name").html(),
 					items: [],
@@ -43,7 +44,7 @@
 					width: e.style.width,
 					height: e.style.height
 				}
-				
+
 				let trs = $(e).find(".list-content tr");
 				for(var i = 0; i < trs.length; i++){
 					let tr = $(trs[i])
@@ -52,13 +53,27 @@
 						checked: Boolean(tr.find(".check input:checked").length)
 					})
 				}
-				
-				list.lists.push(newList);
+
+				board.lists.push(newList);
 			})
-			
-			exportList(list);
+
+			$(".note").toArray().forEach(function(e){
+				e = $(e)[0];
+				let n = (/note-(\d+)/g).exec(e.id)[1] * 1;
+
+				board.notes.push({
+					name: $("#note-" + n + "-name").html(),
+					value: $(e).find("textarea").val(),
+					top: e.style.top,
+					left: e.style.left,
+					width: e.style.width,
+					height: e.style.height
+				});
+			})
+
+			exportBoard(board);
 		}
 	}
-	
-    return exportList;
+
+	return exportBoard;
 }));
--- importBoard.js
+++ importBoard.js
@@ -1,36 +1,36 @@
 (function(root, factory){
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['importList'], function (a) {
-            return (root.amdWebGlobal = factory(a));
-        });
-    } else {
-        // Browser globals
-        root.importList = factory(root.importList);
-    }
-}(typeof window !== 'undefined' ? window : this, function(importList){
-    
-	importList = function(){
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['importBoard'], function (a) {
+			return (root.amdWebGlobal = factory(a));
+		});
+	} else {
+		// Browser globals
+		root.importBoard = factory(root.importBoard);
+	}
+}(typeof window !== 'undefined' ? window : this, function(importBoard){
+
+	importBoard = function(){
 		if($("#board .list").length){
 			if(confirm("Importing will override your existing board, is this okay?")){
-				$("#board .list").remove();
+				$("#board .list,.note").remove();
 			} else {
 				return;
 			}
 		}
-		$('<input id="file-chooser" type="file" onchange="importList.read(this)" accept=".json, .txt" hidden/>')[0].click();
+		$('<input id="file-chooser" type="file" onchange="importBoard.read(this)" accept=".json, .txt" hidden/>')[0].click();
 	}
-	
-	importList.read = function(input){
+
+	importBoard.read = function(input){
 		let file = input.files[0];
 		if(file){
 			let reader = new FileReader();
 			reader.onload = function (e) {
-                createBoard(JSON.parse(e.target.result));
-            };
-            reader.readAsText(file);
+				createBoard(JSON.parse(e.target.result));
+			};
+			reader.readAsText(file);
 		}
 	}
-	
-    return importList;
+
+	return importBoard;
 }));
